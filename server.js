a97d7e56b5de1ef44212dc838b66a00797992586
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-13 10:31:02 -0700

    required mongodb in the server and restructured the server

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const app = require('./lib/app')
-const { createServer } = require('http');
+const http = require('http');
+const app = require('./lib/app');
+require('./lib/mongodb');
 
-createServer(app).listen(3000);
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log('SERVER IS RUNNING ON', server.address().port);
+});
